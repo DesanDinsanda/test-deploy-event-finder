78d0eb301984a30bb5b1a1af5254d4bae1bae2cd
DesanDinsanda <desandinsanda@gmail.com>
2025-12-30 19:41:06 +0530

    width change

--- src/Components/EventCard.tsx
+++ src/Components/EventCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 function EventCard({ event }: Props) {
   return (
     <>
-      <div className="group m-5  bg-[#151e32] border border-slate-700 rounded-xl overflow-hidden text-white w-full  md:w-[320px] lg:w-[300px] xl:w-[320px] 2xl:w-[380px] hover:border-cyan-400 hover:shadow-cyan-400/50 hover:shadow-lg transition-all hover:scale-103 hover:-translate-y-1">
+      <div className="group m-5  bg-[#151e32] border border-slate-700 rounded-xl overflow-hidden text-white w-full  md:w-[320px] lg:w-[300px] xl:w-[320px] 2xl:w-[370px] hover:border-cyan-400 hover:shadow-cyan-400/50 hover:shadow-lg transition-all hover:scale-103 hover:-translate-y-1">
         <div className="relative">
           <img className="contrast-80 w-full h-full" src={event.image} alt="" />
           <p className="absolute top-4 right-2 bg-cyan-300 rounded-xl p-1 w-25 text-cyan-950 font-bold text-sm text-center">
